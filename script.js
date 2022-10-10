a3111aeeb5594cc1178f4df7ae37e1654d20afb3
Alixia <lizziejones1208@gmail.com>
2022-10-11 05:43:17 +0800

    Add a function to adjust grid size

--- script.js
+++ script.js
@@ -6,18 +6,15 @@ const container = document.createElement('div');
 container.setAttribute('class','container');
 document.body.appendChild(container);
 // find a way to overwrite grid values with a prompt
-const grid = {rows: 16, cols: 16};
-const area = (grid.rows * grid.cols);
 
-
-
-function createGrid(total) {
-    for(let i = 0; i < total; i++) {
+function createGrid(area) {
+    for(let i = 0; i < area; i++) {
         const box = document.createElement('div');
         box.classList.add('boxes');
         container.append(box)
         box.addEventListener('mouseover', draw);
     }
+    container.style.setProperty('grid-template-rows', `repeat(${grid.rows}, 1fr)`)
     container.style.setProperty('grid-template-columns', `repeat(${grid.cols}, 1fr)`)
 }
 
@@ -26,9 +23,27 @@ function draw(e) {
     e.target.style.backgroundColor = 'black';
 }
 
+function clearGrid() {
+    container.innerHTML = '';
+}
 
 //make a prompt for new grid size
+btn.addEventListener('click', () => {
+    clearGrid();
+    let rowInput = prompt('Please enter row value');
+    let colInput = prompt('Please enter column value');
+    grid = {rowInput, colInput};
+    area = (grid.rowInput * grid.colInput);
+    for(let i = 0; i < area; i++) {
+        const box = document.createElement('div');
+        box.classList.add('boxes');
+        container.appendChild(box)
+        box.addEventListener('mouseover', draw);
+    }
+    container.style.setProperty('grid-template-rows', `repeat(${grid.rowInput}, 1fr)`)
+    container.style.setProperty('grid-template-columns', `repeat(${grid.colInput}, 1fr)`)
+});
 
-
-
+let grid = {rows: 16, cols: 16};
+let area = (grid.rows * grid.cols);
 createGrid(area);
