cb16703a291ee67b9f1273e1e59bf95815feb0da
Alixia <lizziejones1208@gmail.com>
2022-10-11 05:12:11 +0800

    Add drawing effect using mouseover listener

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const btn = document.createElement('button');
+document.body.append(btn);
+btn.textContent = 'Set new size';
+
 const container = document.createElement('div');
 container.setAttribute('class','container');
 document.body.appendChild(container);
@@ -5,14 +9,26 @@ document.body.appendChild(container);
 const grid = {rows: 16, cols: 16};
 const area = (grid.rows * grid.cols);
 
-createGrid(area);
+
 
 function createGrid(total) {
     for(let i = 0; i < total; i++) {
         const box = document.createElement('div');
-        container.append(box);
-        box.textContent = `${i+1}`;
-        box.setAttribute('class', 'boxes');
+        box.classList.add('boxes');
+        container.append(box)
+        box.addEventListener('mouseover', draw);
     }
     container.style.setProperty('grid-template-columns', `repeat(${grid.cols}, 1fr)`)
 }
+
+//on mouseclick, change class and add a background color to 'draw'
+function draw(e) {
+    e.target.style.backgroundColor = 'black';
+}
+
+
+//make a prompt for new grid size
+
+
+
+createGrid(area);
